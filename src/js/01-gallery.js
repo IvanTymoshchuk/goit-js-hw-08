cff8ba183646119b1df99e48f78778b1b512077c
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-02-23 16:27:25 +0100

    add libraries , css, js for 1 answer

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+const imagesEl = createGalleryImg(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', imagesEl);
+
+function createGalleryImg(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery">
+        <li>
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+</ul>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
