5d91399a204ddb03fc4d50a88a9ee8e9ef70018c
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-03-03 20:27:36 +0100

    test

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,76 @@
-import throttle from 'lodash.throttle';
 import '../css/common.css';
 import '../css/03-feedback.css';
+import throttle from 'lodash.throttle';
 
+// const STORAGE_KEY = 'feedback-form-state';
 
-const STORAGE_KEY = 'feedback-form-state';
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   textarea: document.querySelector('.feedback-form textarea'),
+//   input: document.querySelector('input'),
+// };
+// const formData = {};
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('input'),
-};
-const formData = {};
+// populateTextarea();
 
-populateTextarea();
+// refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
-refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+// refs.form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   const objData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   localStorage.removeItem(STORAGE_KEY);
+// });
 
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
-  e.currentTarget.reset();
-  const objData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  localStorage.removeItem(STORAGE_KEY);
-});
+// function onTextareaInput(e) {
+//   formData[e.target.name] = e.target.value;
+//   const stringifiedData = JSON.stringify(formData);
+//   localStorage.setItem(STORAGE_KEY, stringifiedData);
+// }
 
-function onTextareaInput(e) {
-  formData[e.target.name] = e.target.value;
-  const stringifiedData = JSON.stringify(formData);
-  localStorage.setItem(STORAGE_KEY, stringifiedData);
+// function populateTextarea() {
+//   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+//   if (savedMessage === null) {
+//     return;
+//   }
+//   refs.textarea.value = savedMessage['message'] || '';
+//   refs.input.value = savedMessage['email'] || '';
+// }
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+ refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+// Зберігає значення полів у локальне сховище
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formState));
 }
 
-function populateTextarea() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedMessage === null) {
-    return;
+// Заповнює поля форми збереженими значеннями
+function fillFormFromState() {
+  const formState = JSON.parse(localStorage.getItem(localStorageKey));
+  if (formState) {
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
   }
-  refs.textarea.value = savedMessage['message'] || '';
-  refs.input.value = savedMessage['email'] || '';
 }
+
+// Очищує локальне сховище і поля форми при надсиланні форми
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.removeItem(localStorageKey);
+  emailInput.value = '';
+  messageInput.value = '';
+})
