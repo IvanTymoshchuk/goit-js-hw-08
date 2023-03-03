2819e494918211bf6c947d9bfa79a65d4715177a
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-03-03 21:58:13 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -31,9 +31,16 @@ function onTextareaInput(e) {
 function populateTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (savedMessage === null) {
-    return;
-  }
-  refs.textarea.value = savedMessage['message'] || '';
-  refs.input.value = savedMessage['email'] || '';
+//   if (savedMessage === null) {
+//     return;
+//   }
+//   refs.textarea.value = savedMessage['message'] || '';
+//   refs.input.value = savedMessage['email'] || '';
+
+  if (savedMessage) {
+    formData = savedMessage;
+    refs.textarea.value = savedMessage.['email'] || "";
+    refs.input.value = savedMessage.['message'] || "";
+  };
+};
 }
