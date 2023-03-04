994a20757ddc8fd43699437407582a5f08cf43d1
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-03-04 20:09:24 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,41 +2,41 @@ import '../css/common.css';
 import '../css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
-// const STORAGE_KEY = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-//   textarea: document.querySelector('.feedback-form textarea'),
-//   input: document.querySelector('input'),
-// };
-// const formData = {
-//   email: '',
-//   message: '',
-// };
-// populateTextarea();
-// refs.form.addEventListener('input', throttle(onTextareaInput, 500));
-// refs.form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   localStorage.removeItem(STORAGE_KEY);
-//   e.currentTarget.reset();
-//   console.log(formData);
-// });
-// function onTextareaInput(e) {
-//   formData[e.target.name] = e.target.value;
-//   const stringifiedData = JSON.stringify(formData);
-//   localStorage.setItem(STORAGE_KEY, stringifiedData);
-// }
-// function populateTextarea() {
-//   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-//   if (savedMessage === null) {
-//     return;
-//   }
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('input'),
+};
+const formData = {
+  email: '',
+  message: '',
+};
+populateTextarea();
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+  e.currentTarget.reset();
+  console.log(formData);
+});
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+  const stringifiedData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, stringifiedData);
+}
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage === null) {
+    return;
+  }
 
-//   refs.textarea.value = savedMessage.message || '';
-//   refs.input.value = savedMessage.email || '';
-//   formData.email = savedMessage.email || '';
-//   formData.message = savedMessage.message || '';
-// }
+  refs.textarea.value = savedMessage.message || '';
+  refs.input.value = savedMessage.email || '';
+  formData.email = savedMessage.email || '';
+  formData.message = savedMessage.message || '';
+}
 
 
 //* 2 Варіант
