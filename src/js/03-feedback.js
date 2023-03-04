6f0b64f86230f489f2d12e6e099efccaa1015a8c
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-03-04 14:44:10 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,33 +13,29 @@ const formData = {
   email: '',
   message: '',
 };
-
 populateTextarea();
-
 refs.form.addEventListener('input', throttle(onTextareaInput, 500));
-
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
-  e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  e.currentTarget.reset();
+  console.log(formData);
 });
-
 function onTextareaInput(e) {
   formData[e.target.name] = e.target.value;
   const stringifiedData = JSON.stringify(formData);
   localStorage.setItem(STORAGE_KEY, stringifiedData);
 }
-
 function populateTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
   if (savedMessage === null) {
     return;
   }
+
   refs.textarea.value = savedMessage.message || '';
   refs.input.value = savedMessage.email || '';
-  formData = savedMessage.email || '';
-  formData = savedMessage.message || '';
+  formData.email = savedMessage.email || '';
+  formData.message = savedMessage.message || '';
 }
 
 // const refs = {
@@ -61,10 +57,10 @@ function populateTextarea() {
 
 // function onFormSubmit(e) {
 //   e.preventDefault();
-//   e.currentTarget.reset();
+// localStorage.removeItem(STORAGE_KEY);
 //   formData.email = refs.email.value;
 //   formData.message = refs.message.value;
-//   localStorage.removeItem(STORAGE_KEY);
+//   e.currentTarget.reset();
 // }
 
 // function onFormInput(e) {
